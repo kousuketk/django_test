b33c7233d004183f0c21df0eea7fae71fbc0231c
kousuke takahashi <pamarikomi_t@icloud.com>
2022-05-05 22:26:36 +0900

    create routing crud

--- crud/urls.py
+++ crud/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("health", views.health, name="health")]
+urlpatterns = [
+    path("health/", views.health, name="health"),
+    path("members/", views.index, name="index"),
+    path("members/add/", views.edit, name="add"),
+    path("members/edit/<id>/", views.edit, name="edit"),
+    path("members/delete/<id>/", views.delete, name="delete"),
+    path("members/detail/<id>/", views.detail, name="detail"),
+]
--- crud/views.py
+++ crud/views.py
@@ -4,3 +4,19 @@ from django.http import HttpResponse
 # Create your views here.
 def health(request):
     return HttpResponse("health check.")
+
+
+def index(request):
+    return HttpResponse("一覧")
+
+
+def edit(request, id=None):
+    return HttpResponse("編集")
+
+
+def delete(request, id=None):
+    return HttpResponse("削除")
+
+
+def detail(request, id=None):
+    return HttpResponse("詳細")
